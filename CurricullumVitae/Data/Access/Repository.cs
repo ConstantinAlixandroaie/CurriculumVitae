a3f1b3bff4d9ef8e99c6806d6707b9b96d092a05
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2022-11-19 21:03:00 +0200

    Add project files.

---
+++ CurricullumVitae/Data/Access/Repository.cs
@@ -0,0 +1,38 @@
+﻿using CurricullumVitae.Models;
+using System.Security.Claims;
+
+namespace CurricullumVitae.Data.Access
+{
+    public class Repository<T> : IRepository<T> where T:class, IDbObject
+    {
+        public Task<T> Add(T item, ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> Get(bool asNoTracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id, bool asNoTracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(T item, ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveById(int id, ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(int id, T newData, ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
