e636ccb600ab1807e73a7fe01f7559f22000e48a
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2022-11-20 15:14:34 +0200

    Repositories code initial revision done.

--- CurricullumVitae/Data/Access/EducationRepository.cs
+++ CurricullumVitae/Data/Access/EducationRepository.cs
@@ -1,32 +1,109 @@
 ﻿using CurricullumVitae.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace CurricullumVitae.Data.Access
 {
-    public interface IEducationRepository:IRepository<Education>
+    public interface IEducationRepository : IRepository<Education>
     {
 
     }
-    public class EducationRepository:Repository<Education>, IEducationRepository    
+    public class EducationRepository : Repository<Education>, IEducationRepository
     {
-        public EducationRepository(ApplicationDbContext ctx):base(ctx)
+        public EducationRepository(ApplicationDbContext ctx, ILogger<EducationRepository> logger) : base(ctx, logger)
         {
 
         }
 
-        public override Task<Education> Add(Education item, ClaimsPrincipal user)
+        public override async Task<Education> Add(Education item, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            if (item == null)
+                return null;
+            if (string.IsNullOrEmpty(item.Title))
+                return null;
+            try
+            {
+                var education = new Education
+                {
+                    Title = item.Title,
+                    Description = item.Description,
+                    Document = item.Document,
+                    DocumentId = item.DocumentId,
+                    EndDate = item.EndDate,
+                    StartDate = item.StartDate,
+                    University = item.University,
+                };
+                _ctx.Educations.Add(education);
+                await _ctx.SaveChangesAsync();
+                return education;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
-        public override Task<IEnumerable<Education>> Get(bool asNoTracking = false)
+        public override async Task<IEnumerable<Education>> Get(bool asNoTracking = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var rv = new List<Education>();
+                var sourceCollection = await _ctx.Educations.ToListAsync();
+                foreach (var source in sourceCollection)
+                {
+                    var vm = new Education()
+                    {
+                        Title = source.Title,
+                        Description = source.Description,
+                        Document = source.Document,
+                        DocumentId = source.DocumentId,
+                        EndDate = source.EndDate,
+                        StartDate = source.StartDate,
+
+                    };
+                    rv.Add(vm);
+                }
+                return rv;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
-        public override Task<Education> GetById(int id, bool asNoTracking = false)
+        public override async Task<Education> GetById(int id, bool asNoTracking = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var sourceCollection = _ctx.Educations.AsQueryable();
+                if (asNoTracking)
+                {
+                    sourceCollection = sourceCollection.AsNoTracking();
+                }
+                var education = await sourceCollection.FirstOrDefaultAsync(x => x.Id == id);
+                if (education == null)
+                {
+                    return null;
+                }
+                var rv = new Education()
+                {
+                    Title = education.Title,
+                    Description = education.Description,
+                    Document = education.Document,
+                    DocumentId = education.DocumentId,
+                    EndDate = education.EndDate,
+                    StartDate = education.StartDate,
+
+                };
+                return rv;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public override Task<bool> Remove(Education item, ClaimsPrincipal user)
@@ -34,14 +111,64 @@ namespace CurricullumVitae.Data.Access
             throw new NotImplementedException();
         }
 
-        public override Task RemoveById(int id, ClaimsPrincipal user)
+        public override async Task RemoveById(int id, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var education = await _ctx.Educations.FirstOrDefaultAsync(x => x.Id == id);
+                if (education == null)
+                {
+                    throw new ArgumentNullException(nameof(education));
+                }
+                //authorization
+                _ctx.Educations.Remove(education);
+                await _ctx.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
         }
 
-        public override Task<bool> Update(int id, Education newData, ClaimsPrincipal user)
+        public override async Task<bool> Update(int id, Education newData, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var education = await _ctx.Educations.FirstOrDefaultAsync(x=>x.Id == id);
+                if(education == null) 
+                {
+                    return false;
+                }
+                if (newData.StartDate!=education.StartDate)
+                {
+                    education.StartDate = newData.StartDate;
+                }
+                if (newData.EndDate!= education.EndDate)
+                {
+                    education.EndDate = newData.EndDate;
+                }
+                if (!string.IsNullOrEmpty(newData.Description))
+                {
+                    education.Description = newData.Description;
+                }
+                if (!string.IsNullOrEmpty(newData.Title))
+                {
+                    education.Title = newData.Title;
+                }
+                if (!string.IsNullOrEmpty(newData.University))
+                {
+                    education.University = newData.University;
+                }
+                await _ctx.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
     }
 }
--- CurricullumVitae/Data/Access/ExtraRepository.cs
+++ CurricullumVitae/Data/Access/ExtraRepository.cs
@@ -1,32 +1,106 @@
 ﻿using CurricullumVitae.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace CurricullumVitae.Data.Access
 {
-    public interface IExtraRepository :IRepository<Extra>
+    public interface IExtraRepository : IRepository<Extra>
     {
 
     }
-    public class ExtraRepository:Repository<Extra>,IExtraRepository
+    public class ExtraRepository : Repository<Extra>, IExtraRepository
     {
-        public ExtraRepository(ApplicationDbContext ctx):base(ctx)   
+        public ExtraRepository(ApplicationDbContext ctx, ILogger<ExtraRepository> logger) : base(ctx, logger)
         {
 
         }
 
-        public override Task<Extra> Add(Extra item, ClaimsPrincipal user)
+        public override async Task<Extra> Add(Extra item, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            if (item == null)
+                return null;
+            if (string.IsNullOrEmpty(item.Title))
+                return null;
+            if (item.Document == null)
+                return null;
+            try
+            {
+                var extra = new Extra
+                {
+                    Title = item.Title,
+                    Document = item.Document,
+                    DocumentId = item.DocumentId,
+                    Content = item.Content,
+
+                };
+                _ctx.Extras.Add(extra);
+                await _ctx.SaveChangesAsync();
+                return extra;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
-        public override Task<IEnumerable<Extra>> Get(bool asNoTracking = false)
+        public override async Task<IEnumerable<Extra>> Get(bool asNoTracking = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var rv = new List<Extra>();
+                var sourceCollection = await _ctx.Extras.ToListAsync();
+                foreach (var source in sourceCollection)
+                {
+                    var vm = new Extra
+                    {
+                        Title = source.Title,
+                        Document = source.Document,
+                        DocumentId = source.DocumentId,
+                        Content = source.Content,
+
+                    };
+                    rv.Add(vm);
+                }
+                return rv;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
-        public override Task<Extra> GetById(int id, bool asNoTracking = false)
+        public override async Task<Extra> GetById(int id, bool asNoTracking = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var sourceCollection = _ctx.Extras.AsQueryable();
+                if (asNoTracking)
+                {
+                    sourceCollection = sourceCollection.AsNoTracking();
+                }
+                var extra = await sourceCollection.FirstOrDefaultAsync(x => x.Id == id);
+                if (extra != null)
+                {
+                    return null;
+                    throw new ArgumentNullException(nameof(extra));
+                }
+                var rv = new Extra()
+                {
+                    Title = extra.Title,
+                    Document = extra.Document,
+                    DocumentId = extra.DocumentId,
+                    Content = extra.Content,
+
+                };
+                return rv;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public override Task<bool> Remove(Extra item, ClaimsPrincipal user)
@@ -34,14 +108,47 @@ namespace CurricullumVitae.Data.Access
             throw new NotImplementedException();
         }
 
-        public override Task RemoveById(int id, ClaimsPrincipal user)
+        public override async Task RemoveById(int id, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var extra = await _ctx.Extras.FirstOrDefaultAsync(x => x.Id == id);
+                if (extra == null)
+                    throw new ArgumentNullException(nameof(extra));
+                _ctx.Extras.Remove(extra);
+                await _ctx.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
-        public override Task<bool> Update(int id, Extra newData, ClaimsPrincipal user)
+        public override async Task<bool> Update(int id, Extra newData, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var extra = await _ctx.Extras.FirstOrDefaultAsync(x=>x.Id== id);
+                if (extra == null)
+                    return false;
+                if (!string.IsNullOrEmpty(newData.Title))
+                {
+                    extra.Title = newData.Title;
+                }
+                if(!string.IsNullOrEmpty(newData.Content))
+                {
+                    extra.Content = newData.Content;
+                }
+                await _ctx.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
     }
 }
